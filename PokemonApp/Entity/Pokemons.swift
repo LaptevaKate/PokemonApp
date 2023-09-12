3e5d7c4c2a6bdb05c012e367500708b2f894ad20
LaptevaKate <ekaterina213@yandex.ru>
2023-09-12 16:24:12 +0300

    created entity for pokemon model

---
+++ PokemonApp/Entity/Pokemons.swift
@@ -0,0 +1,20 @@
+//
+//  PokemonList.swift
+//  PokemonApp
+//
+//  Created by Екатерина Лаптева on 12.09.23.
+//
+
+import Foundation
+
+struct Pokemons: Codable {
+    
+    let results: [PokemonListItem]
+}
+
+struct PokemonListItem: Codable {
+    
+    let name: String
+    let url: String
+    let id: Int
+}
